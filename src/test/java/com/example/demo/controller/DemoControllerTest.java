09293189cf707b0c768cb776b46d4d2e7b315d0d
hytsnbr10519 <haya524mwam@gmail.com>
2023-01-18 23:15:23 +0900

    Test

---
+++ src/test/java/com/example/demo/controller/DemoControllerTest.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DemoControllerTest {
+
+  @Test
+  void testIndex() {
+    assertEquals("", "");
+  }
+}
