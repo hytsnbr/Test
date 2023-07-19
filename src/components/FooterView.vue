4428e799fa54b2e112c1ca27d7d3a308773e55fd
hytsnbr10519 <haya524mwam@gmail.com>
2023-07-19 23:19:55 +0900

    test

--- src/components/FooterView.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <footer class="bottom-0 p-2 shadow-inner">
-    <div class="flex place-content-center place-items-center">
-      <p class="">© 2023 hytsnbr</p>
-    </div>
-  </footer>
-</template>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import App from "@/App.vue";
 import "@/scss/lib.scss";
 import "@/scss/style.scss";
-import { createRouter } from "@/router";
 import { createApp } from "vue";
 
-createApp(App).use(createRouter("history")).mount("#app");
+createApp(App).mount("#app");
